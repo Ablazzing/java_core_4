c81b9fe4fa2fc828252fe5c3c01909e748d9adb9
YMolodyko <VTB4081847@region.vtb.ru>
2024-08-15 18:13:53 +0300

    lesson 7

---
+++ src/org/javaacademy/core/lesson6/ex2/Cat.java
@@ -0,0 +1,12 @@
+package org.javaacademy.core.lesson6.ex2;
+
+public class Cat extends Animal {
+
+    public Cat(int weight) {
+        super(weight);
+    }
+
+    public void makeSound() {
+        System.out.println("Мяу");
+    }
+}
